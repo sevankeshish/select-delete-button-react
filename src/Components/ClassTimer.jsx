c38ab6253c34e3c4176a86bf79c38f1e2cd4d68e
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2022-02-21 11:07:48 +0330

    CDU and CWUM in class components

--- src/Components/ClassTimer.jsx
+++ src/Components/ClassTimer.jsx
@@ -4,6 +4,19 @@ export default class ClassTimer extends Component {
   state = {
     count: 0,
   };
+
+  componentDidMount() {
+    this.myTimer = setInterval(() => {
+      console.log("Component Did Mount");
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    console.log("CWUM");
+    clearInterval(this.myTimer);
+  }
+
   render() {
     return (
       <div>
